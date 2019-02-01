8d28f0719730e7a24d6121a79db3e2c1276032d3
Esdras Pavon <pavonesdras@gmail.com>
2019-02-01 09:07:35 -0300

    initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+    appName: 'mymusic',
+    apiKey: '1fe8792bdbb1ba9e4738e8b965cce860',
+    secret: '7de59f9614b1a6b9a77f731ecdd3e218',
+    registeredTo: 'esdraspavon'
+}
+
+// Application name	mymusic
+// API key	1fe8792bdbb1ba9e4738e8b965cce860
+// Shared secret	7de59f9614b1a6b9a77f731ecdd3e218
+// Registered to	esdraspavon
